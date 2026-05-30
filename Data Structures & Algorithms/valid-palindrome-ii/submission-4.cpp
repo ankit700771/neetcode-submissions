fe06c5528e4034a098f2dbccf07bc8d1f55f8ea5
Ankit Gupta <65904396+ankit700771@users.noreply.github.com>
2026-05-30 22:28:27 +0530

    Add: valid-palindrome-ii - submission-4

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-4.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    bool checkPalindrome(string s, int i, int j) {
+        int start = i;
+        int end = j;
+
+        while(start < end) {
+            if(s[start] != s[end]) return false;
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+
+        if(s.size() == 1) return true;
+
+        int start = 0;
+        int end = s.size() - 1;
+
+        while(start <= end) {
+            if(s[start] == s[end]) {
+                start++;
+                end--;
+            } else {
+                bool ans1 = checkPalindrome(s, start+1, end);
+                bool ans2 = checkPalindrome(s, start, end-1);
+
+                if(ans1 || ans2) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
