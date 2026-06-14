5625df55d064ea262b068ae7a8db3b8b71cf466f
Ankit Gupta <65904396+ankit700771@users.noreply.github.com>
2026-06-14 15:37:43 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    vector<int> getLeftMaxArray(vector<int>& height , int &n) {
+        vector<int>leftMax(n);
+        leftMax[0] = height[0];
+        for(int i = 1; i<n; i++) {
+            leftMax[i] = max(leftMax[i-1], height[i]);
+        }
+        return leftMax;
+    }
+
+    vector<int> getReightMaxArray(vector<int>& height , int &n) {
+        vector<int>rightMax(n);
+        rightMax[n-1] = height[n-1];
+        for(int i = n-2; i>=0; i--) {
+            rightMax[i] = max(rightMax[i+1], height[i]);
+        }
+        return rightMax;
+    }
+
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int sum = 0;
+        vector<int>leftMax = getLeftMaxArray(height, n);
+        vector<int>rightMax = getReightMaxArray(height, n);
+
+        for(int i = 0; i<n; i++) {
+            sum += min(leftMax[i], rightMax[i]) - height[i];
+        }
+
+        return sum;
+    }
+};
