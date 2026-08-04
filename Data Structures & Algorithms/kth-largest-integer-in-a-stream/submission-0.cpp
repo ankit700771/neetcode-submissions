6f4900a652dec3f321a88f49d2061e19b337bce2
Ankit Gupta <65904396+ankit700771@users.noreply.github.com>
2026-08-04 11:40:22 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,27 @@
+class KthLargest {
+private:
+    int k;
+    priority_queue<int, vector<int>, greater<int>> pq;
+public:
+
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(auto num: nums) {
+            pq.push(num);
+
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+
+        if(pq.size() > k) {
+            pq.pop();
+        }
+
+        return pq.top();
+    }
+};
